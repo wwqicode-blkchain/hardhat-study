d318e2d3ce2c8bb00635f9c7e2d72b63c83d40f7
wwqicode-blkchain <wangwenqiliu@gmail.com>
2023-10-25 11:28:33 +0800

    feat(scripts.deploy): trying to deploy on mantle testnet

---
+++ hardhat.config.js
@@ -0,0 +1,22 @@
+require("@nomicfoundation/hardhat-toolbox");
+/** @type import('hardhat/config').HardhatUserConfig */
+
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+const ALCHEMY_API_KEY = "3eHXh2byYuCGtwphlF_n8xVKhDw1KJKe";
+
+// Replace this private key with your Goerli account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Beware: NEVER put real Ether into testing accounts
+const MANTLETEST_PRIVATE_KEY = "a4b648aa2c9ba1369817ca16cd6eaaead9c2afb17360dbbca2e24b839d080703";
+
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    mantletest: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [MANTLETEST_PRIVATE_KEY]
+    }
+  }
+};
